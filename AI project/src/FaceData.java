63fcf49a92d07a1711d2316c75404cf78f149cf1
schristian <samchristian8@gmail.com>
2018-04-26 21:13:59 -0400

    Change the file paths of the data to wherever they are on your computer

---
+++ AI project/src/FaceData.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FaceData {
+	int valid;
+	
+	Map<Integer, Integer> lineData=new HashMap();
+	
+	public FaceData(int valid){
+		this.valid=valid;
+	}
+	
+	
+}
